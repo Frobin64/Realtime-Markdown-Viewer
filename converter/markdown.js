7ff127fb168647d921cd6d85bfe0e7d360fd5b42
Robin <faverjonr8@gmail.com>
2024-09-26 11:17:16 +0200

    replace strong with bold and add proper strong definition in parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -2,6 +2,15 @@ var parseBold = function(str) {
   var boldRegExp = /(\*\*)(.*?)\1/;
   var stra = [];
   while ((stra = boldRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<b>' + stra[2] + '</b>')
+  }
+  return str;
+ }
+
+var parseStrong = function(str) {
+  var strongRegExp = /(~~)(.*?)\1/;
+  var stra = [];
+  while ((stra = strongRegExp.exec(str)) !== null) {
     str = str.replace(stra[0], '<strong>' + stra[2] + '</strong>')
   }
   return str;
